f352112bf89cadb72a322e21ac19a8237596ed98
Hafiyuddin Azhad <hafiyuddinazhadhassan@gmail.com>
2024-05-02 23:15:56 +0800

    update url path

--- webguard/urls.py
+++ webguard/urls.py
@@ -1,8 +1,9 @@
-# webguard/urls.py
+# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),  # Define the root URL pattern
     path('second-page/', views.second_page, name='second_page'),
 ]
